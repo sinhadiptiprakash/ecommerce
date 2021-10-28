57d2dfe5aadd7108addb0c677a0ae3ef10062be3
Dipti Prakash Sinha <sinhadiptiprakash@gmail.com>
2021-10-28 12:28:24 +0530

    api creation, nodemon install, node update

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            price: 100,
+            countInStock: 1,
+            image: '/images/p6.jpeg',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'A red t-shirt, perfect for days with non-red weather.'
+        },
+        {
+            id: '2',
+            name: 'Woodland Jeans pant 2',
+            category: 'Pants',
+            price: 510,
+            countInStock: 10,
+            image: '/images/p6.jpeg',
+            brand: 'Woodland Jeans',
+            rating: 3.0,
+            numReviews: 10,
+            description: 'A red t-shirt, perfect for days with non-red weather.'
+        },
+        {
+            id: '3',
+            name: 'Nike Slim Shirt 3',
+            category: 'Shirts',
+            price: 190,
+            countInStock: 0,
+            image: '/images/p6.jpeg',
+            brand: 'Nike',
+            rating: 2.6,
+            numReviews: 10,
+            description: 'A red t-shirt, perfect for days with non-red weather.'
+        },
+        {
+            id: '4',
+            name: 'Nike Slim Pant 4',
+            category: 'Pants',
+            price: 120,
+            countInStock: 100,
+            image: '/images/p6.jpeg',
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 100,
+            description: 'A red t-shirt, perfect for days with non-red weather.'
+        },
+        {
+            id: '5',
+            name: 'Peter England Slim Pant 5',
+            category: 'Pants',
+            price: 100,
+            countInStock: 5,
+            image: '/images/p6.jpeg',
+            brand: 'Peter England',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'A red t-shirt, perfect for days with non-red weather.'
+        },
+        {
+            id: '6',
+            name: 'Adidas Slim Shirt 6',
+            category: 'Shirts',
+            price: 130,
+            countInStock: 0,
+            image: '/images/p6.jpeg',
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'A red t-shirt, perfect for days with non-red weather.'
+        },
+    ]
+}
+
+export default data;
