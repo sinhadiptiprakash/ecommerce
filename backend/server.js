1d723a8ee8484dbcbe94b2253b6af488e05e3679
Dipti Prakash Sinha <sinhadiptiprakash@gmail.com>
2021-10-29 10:39:48 +0530

    Loading and MessageBox implementation

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) =>
 {
     res.send("Server is ready");
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () =>
 {
     console.log(`server is running at https://localhost:${port}`);
